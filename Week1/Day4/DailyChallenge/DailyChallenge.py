dc892965677c333b44bb2dff7440943c9aa22ee5
Devi000 <levylleremi@gmail.com>
2025-12-18 21:25:27 +0200

    w1 d5

--- Week1/Day4/DailyChallenge/DailyChallenge.py
+++ Week1/Day4/DailyChallenge/DailyChallenge.py
@@ -12,12 +12,12 @@
 # 2. Create a program that generates a list of multiples of the given number.
 # 3. The list should stop when it reaches the length specified by the user.
 
-number = int(input("Enter a number: "))
-length = int(input("Enter length: "))
-multiples = []
-for i in range(1, length + 1):
-    multiples.append(number * i)
-print(multiples)
+# number = int(input("Enter a number: "))
+# length = int(input("Enter length: "))
+# multiples = []
+# for i in range(1, length + 1):
+#     multiples.append(number * i)
+# print(multiples)
 
 # Challenge 2: Remove Consecutive Duplicate Letters
 # Key Python Topics:
@@ -32,11 +32,11 @@ print(multiples)
 # The new string should only contain unique consecutive letters.
 # For example, “ppoeemm” should become “poem” (removes consecutive duplicates like ‘pp’, ‘ee’, and ‘mm’).
 # 3. The program should print the modified string.
-repetido = input("Smash your keyboard or wtv: ")
-limpio = ""
+repeated = input("Smash your keyboard or wtv: ")
+cleaned = ""
 checker = ""
-for i in repetido:
+for i in repeated:
     if i != checker:
-        limpio += i
+        cleaned += i
         checker = i
-print(limpio)
+print(cleaned)
